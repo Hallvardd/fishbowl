3fd5a948fa79a78ed6e36c6bc6c5b3d160b6a5f0
hallvardd <hallvard.stemshaug@gmail.com>
2025-03-06 16:51:15 +0100

    fix icon path

--- fishbowl/src/components/UploadIcon.vue
+++ fishbowl/src/components/UploadIcon.vue
@@ -1,13 +1,13 @@
 <template>
-  <ArrowUpIcon class="w-6 h-6 text-blue-500" />
+  <ArrowUpTrayIcon class="w-6 h-6 text-blue-500" />
 </template>
 
 <script>
-import { ArrowUpIcon } from '@heroicons/vue/outline';
+import { ArrowUpTrayIcon } from '@heroicons/vue/24/outline';
 
 export default {
   components: {
-    ArrowUpIcon
+    ArrowUpTrayIcon
   }
 };
 </script>
